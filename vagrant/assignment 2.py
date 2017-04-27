f70efef5caaa2dbf53023fb7e1fc821393890802
GouthamML <goutham24gml@gmail.com>
2017-04-27 11:37:01 +0530

    recent!

---
+++ vagrant/assignment 2.py
@@ -0,0 +1,49 @@
+import httplib2
+import json
+import sys
+import codecs
+
+def getGeocodeLocation(inputString):
+    # Use Google Maps to convert a location into Latitute/Longitute coordinates
+    # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+    google_api_key = "PASTE_YOUR_KEY_HERE"
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    h = httplib2.Http()
+    result = json.loads(h.request(url,'GET')[1])
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude,longitude)
+
+
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+foursquare_client_id = "A5SLLJZFCW5YJH1OFDSJ2ANDKBGFQIJZGENOEZTMITSY5LJS"
+foursquare_client_secret = "AAT153KQCIEG3CHYX5JLVMCZ5UBQH4FCVA0LXSVKAYJFBNFI"
+
+
+def findARestaurant(mealType,location):
+	url = ('venues/search?ll=%s,%s&query=%s,browse'(latitude,longitude,mealType,location))
+	
+	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
+	
+	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
+	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+
+	#3. Grab the first restaurant
+	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+	#5. Grab the first image
+	#6. If no image is available, insert default a image url
+	#7. Return a dictionary containing the restaurant name, address, and image url	
+if __name__ == '__main__':
+	findARestaurant("Pizza", "Tokyo, Japan")
+	# findARestaurant("Tacos", "Jakarta, Indonesia")
+	# findARestaurant("Tapas", "Maputo, Mozambique")
+	# findARestaurant("Falafel", "Cairo, Egypt")
+	# findARestaurant("Spaghetti", "New Delhi, India")
+	# findARestaurant("Cappuccino", "Geneva, Switzerland")
+	# findARestaurant("Sushi", "Los Angeles, California")
+	# findARestaurant("Steak", "La Paz, Bolivia")
+	# findARestaurant("Gyros", "Sydney Australia")
